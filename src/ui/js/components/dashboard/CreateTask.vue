a9b4b9c334fed76414865dcb2d15247d02d87521
mariusAlc <mannye.skps@gmail.com>
2018-10-01 17:09:08 +0300

    TaskCreator

--- src/ui/js/components/dashboard/CreateTask.vue
+++ src/ui/js/components/dashboard/CreateTask.vue
@@ -5,27 +5,27 @@
 		<p>This is the task creator!</p>
 		<form>
 			<div class="form-group">
-				<input id="task-Title" type="text" class="form-control input-sm chat-input"  placeholder="Task Title" v-model="taskInfo.taskTitle" />
+				<input id="task-Title" type="text" class="form-control input-sm chat-input"  placeholder="Task Title" v-model="taskName" />
 			</div>
 			<div class="form-group">
-				<textarea name="message" rows="10" cols="30" v-model="taskInfo.taskString">Input your task.</textarea>
+				<textarea name="message" rows="10" cols="30" v-model="taskString">Input your task.</textarea>
 			</div>
 			<div class="form-group">
-				<input id="user-Rec" type="text" class="form-control input-sm chat-input"  placeholder="Designated User" v-model="userRec" />
+				<input id="user-Rec" type="text" class="form-control input-sm chat-input"  placeholder="Designated User" v-model="usernameReceiver" />
 			</div>
 				<form id="options">
-					<select>
-					<div>Group</div><option v-for="(item, index) in this.user.groupNames" :key="index">{{item}}</option>
+					<select v-model="groupName">
+					<div>Group</div><option v-for="(item, index) in this.user.groupNames" :key="index" :value="item">{{item}}</option>
 					</select><br>
 
 					<div>DeadLine</div><input type="checkbox" @click="changeDeadline()">
-					<input v-if="this.checkboxDeadline" type="date" name="DeadLine"><br>
+					<input v-if="checkboxDeadline" type="date" name="DeadLine"><br>
 
 					<div>Status</div><input type="checkbox" @click="changeStatus()">
-					<select v-if="this.checkboxStatus">
-						<option name="Urgent">Urgent</option>
-						<option name="Moderate">Moderate</option>
-						<option name="At leisure">At leisure</option>
+					<select v-if="checkboxStatus" v-model="taskPriority">
+						<option name="Urgent" value="urgent">Urgent</option>
+						<option name="Moderate" value="moderate">Moderate</option>
+						<option name="At leisure" value="atLeisure">At leisure</option>
 					</select>
 				</form>
 			<button type="submit" @click="submitTask">Create Task</button>
@@ -42,12 +42,15 @@ module.exports = {
 	
 	data() {
 		return {
-			checkboxStatus:false,
-			checkboxDeadline:false,
-			taskInfo:{
-				taskTitle:'',
-				taskString:'',
-			}
+			checkboxStatus: false,
+			checkboxDeadline: false,
+			usernameCreator:'',
+			usernameReceiver:'',
+			groupName:'',
+			taskName:'',
+			taskString:'',
+			taskPriority:'',
+
 		};
 	},
 
@@ -60,13 +63,25 @@ module.exports = {
 	methods: {
 		changeDeadline: function() {
 			this.checkboxDeadline = !this.checkboxDeadline;
+			console.log(this.user.username);
+			console.log(this.usernameReceiver);
+			console.log(this.groupName);
+			console.log(this.taskName);
+			console.log(this.taskString);
+			console.log(this.taskPriority);
+			
 		},
 		changeStatus: function() {
 			this.checkboxStatus = !this.checkboxStatus;
 		},
 		async submitTask(){
 			await this.$store.dispatch ('user/sendTask', {
-				taskInfo:this.taskInfo,
+				usernameCreator:this.user.username,
+				usernameReceiver:this.usernameReceiver,
+				groupName:this.groupName,
+				taskName:this.taskName,
+				taskString:this.taskString,
+				taskPriority:this.taskPriority,
 			});
 		},
 	}
